2d7a1869004b0e1ed9ffacfdd0edb2590927cdb1
nadaaputri <144821759+nadaaputri@users.noreply.github.com>
2024-10-16 12:50:52 +0700

    Add files via upload
    latihan2

---
+++ pertemuan4/latihan2/soal1.sql
@@ -0,0 +1,7 @@
+SELECT *
+FROM [dbo].[mahasiswa]
+WHERE tahun_masuk
+BETWEEN 2019 AND 2020;
+
+SELECT *
+FROM [dbo].[mahasiswa];
---
+++ pertemuan4/latihan2/soal8.sql
@@ -0,0 +1,4 @@
+SELECT *
+FROM [dbo].[mahasiswa]
+WHERE [jurusan] = 'Teknik Informatika' AND [alamat] LIKE '%Jl.Sudirman%'
+BETWEEN '2018' 
